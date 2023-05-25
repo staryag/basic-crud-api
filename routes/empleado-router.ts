1d3f29e9bad697a4371a14e97188853319af8b8b
staryag <staryag@gmail.com>
2023-05-25 09:32:03 +0200

    Estructura del proyecto

--- routes/empleado-router.ts
+++
@@ -1,8 +0,0 @@
-import {empleadoController} from '../controllers/empleado-controller';
-import {Router} from 'express';
-
-const empleadoRouter: Router = Router();
-
-empleadoRouter.route("/create").post(empleadoController.nuevoEmpleado);
-
-export {empleadoRouter};
